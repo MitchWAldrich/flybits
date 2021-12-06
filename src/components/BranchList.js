818739af754fe54f45f05002b12534d69872ac18
MitchWAldrich <mitch.w.aldrich@gmail.com>
2021-12-06 02:38:37 -0500

    add branch delete functionality

--- src/components/BranchList.js
+++ src/components/BranchList.js
@@ -2,12 +2,14 @@ import React from "react";
 import BranchListItem from "./BranchListItem";
 
 export default function BranchList(props) {
-  const { branches } = props;
+  const { branches, onDelete } = props;
 
   const parsedBranches = branches.map(data =>
     <BranchListItem
     key={data.id}
+    id={data.id}
     name={data.name}
+    onDelete={onDelete}
     />
     )
 
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 export default function Button(props) {
-  const { text, type } = props;
+  const { text, type, onDelete } = props;
   return <>
-  <button type={type}>
+  <button type={type} onClick={onDelete}>
     {text}
   </button>
   </>
