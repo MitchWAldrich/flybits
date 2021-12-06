1b2007003c753d3bce96dd9fcca5c7f2099b6722
MitchWAldrich <mitch.w.aldrich@gmail.com>
2021-12-06 01:12:43 -0500

    create component for new branch creation form

---
+++ src/components/Create.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+import Button from "./Button";
+
+export default function Create(props) {
+  const { onSubmit } = props;
+  const [name, setName] = useState("");
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    onSubmit(name, latitude, longitude)
+    console.log('submitted')
+  }
+
+  return (
+    <main>
+    <form autoComplete="off" onSubmit={handleSubmit}>
+      <input
+        name="name"
+        type="text"
+        placeholder="Enter Branch Name"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
+      <input
+        name="latitude"
+        type="number"
+        placeholder="Enter Latitude"
+        value={latitude}
+        onChange={(event) => setLatitude(event.target.value)}
+      />
+      <input
+        name="longitude"
+        type="number"
+        placeholder="Enter Longitude"
+        value={longitude}
+        onChange={(event) => setLongitude(event.target.value)}
+      />
+      <Button
+      text="Submit"
+      // onClick={onSubmit(name, latitude, longitude)}
+    />
+    </form>
+    </main>
+  )
+}
